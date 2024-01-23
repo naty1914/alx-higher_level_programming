39a148b2617e5c46052c3434b95a2c281bfa2265
Natnael Seifu <natiseifu02@gmail.com>
2024-01-22 18:36:54 -0800

    second commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        element = int(x)
-    except ValueError as e:
-        print(f"error: {e}")
-    countNumber = 0
+        x = int(x)
+    except ValueError:
+        print(f"Error: {x} is not a valid integer.")
+        return 0
+
+    count = 0
     for element in my_list[0:x]:
         print(element, end='')
-        countNumber = countNumber +1
+        count += 1
     print()
-    return element
+    return count
