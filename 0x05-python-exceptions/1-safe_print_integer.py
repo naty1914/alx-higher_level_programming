e8f84ea8d7e09c05e91d489dc3fedc8e74da23ac
Natnael Seifu <natiseifu02@gmail.com>
2024-01-22 16:38:14 -0800

    second commit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
+        if isinstance(value, int):
+            print("{}".format(value))
+            return True
+    except ValueError:
         return False
