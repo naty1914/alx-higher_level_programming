32eab589d96cee710e51becfd12c10ffab53a7da
Natnael Seifu <natiseifu02@gmail.com>
2024-01-24 17:32:24 -0800

    fourth commit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,8 +4,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        resultOfFunction = fct(*args)
-        return resultOfFunction
+        result = fct(*args)
+        return result
     except Exception as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
         return None
