0089f8b871c55a98ab07c3b545406060efbd5c44
Natnael Seifu <natiseifu02@gmail.com>
2024-03-14 15:52:52 -0700

    first

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- A script that creates the table force_name with id and name description
+CREATE TABLE IF NOT  EXISTS force_name (
+id INT,
+name VARCHAR(256) NOT NULL
+);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- A script that creates the table id_not_null  with description of id and name
+CREATE TABLE IF NOT EXISTS id_not_null (
+id INT DEFAULT 1,
+name VARCHAR(256)
+);
