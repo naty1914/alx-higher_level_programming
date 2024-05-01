8734c9fc4bef396e1f3d6006e99248942a8a0aea
Natnael Seifu <natiseifu02@gmail.com>
2024-04-30 19:22:40 -0700

    first

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" A script  that fetches https://alx-intranet.hbtn.io/status
+using urllib and displays the body of the response
+"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html_body = response.read()
+    
+    print("Body response:")
+    print("\t- type:", type(html_body))
+    print("\t- content:", html_body)
+    print("\t- utf8 content:", html_body.decode('utf-8'))
